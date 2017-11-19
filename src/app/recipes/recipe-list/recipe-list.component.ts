3f562a03be2fb8540eb16c793fdbfe48ac622c0f
Kalil Peixoto <kalilmvp@gmail.com>
2017-11-19 19:04:47 +0100

    Html for the components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes:Recipe[] = [
+    new Recipe("Recipe 1", "Description 1", "http://img1.cookinglight.timeinc.net/sites/default/files/updated_main_images/1201p124-chicken-piccata-x.jpg"),
+    new Recipe("Recipe 2", "Description 2", "http://img1.cookinglight.timeinc.net/sites/default/files/updated_main_images/1201p124-chicken-piccata-x.jpg")
+  ];
 
   constructor() { }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredients } from '../shared/ingredients.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients:Ingredients[] = [
+    new Ingredients("Ingredient 1", 50.4),
+    new Ingredients("Ingredient 2", 100.4),
+    new Ingredients("Ingredient 3", 61.1),
+    new Ingredients("Ingredient 4", 97.2),
+    new Ingredients("Ingredient 5", 65.4)
+  ];
 
   constructor() { }
 
